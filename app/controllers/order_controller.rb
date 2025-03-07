aa75b1acdfc5d06b6dcca2dda31c30734c0a0704
vinicius zamprogno <vinicius.quimica@gmail.com>
2025-03-07 17:15:13 -0300

    show logic

--- app/controllers/order_controller.rb
+++ app/controllers/order_controller.rb
@@ -1,2 +1,21 @@
 class OrderController < ApplicationController
+  before_action :set_power, only: [:show]
+  before_action :set_order, only: [:show]
+
+  def show
+    if order.user == current_user
+      render :show
+    else
+      redirect_to root_path, alert: "Nao autorizado"
+    end
+
+    private
+
+    def set_order
+      @order = Order.find(params[:id])
+    end
+
+    def set_power
+      @power = Power.find(params[:id])
+    end
 end
