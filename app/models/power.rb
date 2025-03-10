c55b5dbe32a5da883c6b836f316eab174f910cb0
Marcio Vianna <mvianna.1994@gmail.com>
2025-03-10 10:44:36 -0300

    Add dotenv-rails

--- app/models/power.rb
+++ app/models/power.rb
@@ -1,6 +1,6 @@
 class Power < ApplicationRecord
   belongs_to :user
-
+  has_one_attached :photo
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :description, presence: true
